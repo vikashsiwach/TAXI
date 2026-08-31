5a4907fe218bfb39de39b4525397d292bcb4b1c2
vikashsiwach <siwachvikash70@gmail.com>
2026-08-31 23:25:52 +0530

    fixed captain auth

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -3,6 +3,12 @@ const captainModel = require('../models/captain.model');
 const captainService = require('../services/captain.service');
 const {validationResult} = require('express-validator');
 const blacklistTokenModel = require('../models/blacklistToken.model');
+const { userCookieOptions } = require('../utils/cookieOptions');
+
+const captainCookieOptions = {
+  ...userCookieOptions,
+  httpOnly: true,
+};
 
 module.exports.registerCaptain = async (req, res, next) =>{
 
@@ -12,11 +18,10 @@ module.exports.registerCaptain = async (req, res, next) =>{
   }
 
   const {fullname, email, password, vehicle} = req.body;
-  console.log(req.body)
   const isCaptainAlreadyExist = await captainModel.findOne({email});
 
   if (isCaptainAlreadyExist) {
-    return res.status(400).json({message: 'Captain with this email already exists' });
+    return res.status(400).json({ message: 'Captain already exists' });
   }
 
   const hashedPassword = await captainModel.hashPassword(password);
@@ -34,6 +39,8 @@ module.exports.registerCaptain = async (req, res, next) =>{
 
   const token = captain.generateAuthToken();
 
+  res.cookie('captainToken', token, captainCookieOptions);
+
   res.status(201).json({token, captain});
 }
 
@@ -49,17 +56,17 @@ module.exports.loginCaptain = async (req, res, next) =>{
 
   const captain = await captainModel.findOne({email}).select('+password');
   if (!captain){
-    return res.status(401).json({message: 'Invalid email or password'});
+    return res.status(404).json({ message: 'Captain not registered' });
   }
 
   const isMatch = await captain.comparePassword(password);
   if (!isMatch){
-    return res.status(401).json({message: 'Invalid email or password'});
+    return res.status(401).json({ message: 'Invalid password' });
   }
 
   const token = captain.generateAuthToken();
   
-  res.cookie('token', token);
+  res.cookie('captainToken', token, captainCookieOptions);
 
   res.status(200).json({token, captain});
 }
@@ -69,11 +76,13 @@ module.exports.getCaptainProfile = async (req, res, next) =>{
 }
 
 module.exports.logoutCaptain = async (req, res, next) =>{
-  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+  const token = req.cookies.captainToken || req.headers.authorization?.split(' ')[1];
 
-  await blacklistTokenModel.create({token});
+  if (token) {
+    await blacklistTokenModel.create({token});
+  }
 
-  res.clearCookie('token');
+  res.clearCookie('captainToken');
 
   res.status(200).json({message: 'Logged out successfully'});
 }
